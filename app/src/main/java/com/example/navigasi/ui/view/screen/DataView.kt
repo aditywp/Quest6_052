ebb27885cdcf07fa75511233461240121824e75b
adityapermono8 <adityapermono8@gmail.com>
2024-11-22 16:11:16 +0700

    Membuat Fungsi DataView

---
+++ app/src/main/java/com/example/navigasi/ui/view/screen/DataView.kt
@@ -0,0 +1,14 @@
+package com.example.navigasi.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import com.example.navigasi.model.Mahasiswa
+import com.example.navigasi.model.RencanaStudi
+
+
+@Composable
+fun DataView(
+    dataMHS: Mahasiswa,
+    dataKRS: RencanaStudi,
+    onBackButtonClicked: () -> Unit
+) {
+}
