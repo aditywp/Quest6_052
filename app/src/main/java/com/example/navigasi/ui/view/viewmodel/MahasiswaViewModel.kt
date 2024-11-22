6edcaf5e68dad70977caaf16b4a854865fdd34f0
adityapermono8 <adityapermono8@gmail.com>
2024-11-22 15:47:27 +0700

    menambahkan import update

--- app/src/main/java/com/example/navigasi/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/navigasi/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -5,6 +5,7 @@ import com.example.navigasi.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel : ViewModel() {
     private val _dataModel = MutableStateFlow(Mahasiswa())
